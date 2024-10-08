cd9ae548521470adb52a2885547052759cac869f
silv0007 <kauacacula46@gmail.com>
2024-10-08 03:10:37 -0300

    back-end

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: "localhost",
+    dialect: 'mysql',
+    username: 'root',
+    password: '',
+    database: 'express_sequelize',
+    define: {
+        timeStamp: true,
+        underscored: true,
+    },
+
+};
